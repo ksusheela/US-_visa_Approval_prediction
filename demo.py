49630324b4e48afdc825cfda0cd4796cf1b246e3
Susheela kothakonda <sushe9sushe@gmail.com>
2024-03-03 00:14:58 +0100

    mongodb connection file created

--- demo.py
+++ demo.py
@@ -1,9 +1,2 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
-
-try:
-    a = 1 / "10"
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys) from e
+from us_visa.constants import *
+print(COLLECTION_NAME)  
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-
+from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
